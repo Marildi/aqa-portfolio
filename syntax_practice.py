82c9f732133ff5598b5ac116679f7d100a235aaf
Marildi <mdidyk.tech@gmail.com>
2026-07-24 13:10:52 -0400

    Step 10: List/dict comprehensions

--- syntax_practice.py
+++ syntax_practice.py
@@ -79,43 +79,77 @@
 
 # print(check_security_code("Poseidon"))
 
-def check_pilots_count(number):
-    return number > 1
+# def check_pilots_count(number):
+#     return number > 1
 
-print(check_pilots_count(4))
-
-
-#dosctrings
-def check_security_code_advanced(securityCodeWord: str) -> bool:
-    """Return True if the given security code matches 'Atlantis'."""
-    return securityCodeWord == "Atlantis"
-
-print(check_security_code_advanced("Atlantis"))
-
-#Default arguments
-def check_max_distance(distance, threshold=1000):
-    return distance > threshold
-
-print(check_max_distance(11000))
-print(check_max_distance(999))
+# print(check_pilots_count(4))
 
 
-#*args
-def check_every_pilot_identity(*pilots):
-    for pilot in pilots:
-        print(f"{pilot} is confirmed")
+# #dosctrings
+# def check_security_code_advanced(securityCodeWord: str) -> bool:
+#     """Return True if the given security code matches 'Atlantis'."""
+#     return securityCodeWord == "Atlantis"
 
-check_every_pilot_identity("Goodchild", "Chuck", "Mitch", "Dakota")
+# #Default arguments
+# def check_max_distance(distance, threshold=1000):
+#     return distance > threshold
 
-#*kwargs
-def report_shell_temperature(**degrees):
-    for star_proximity, value in degrees.items():
-        print(f"{star_proximity}: {value}")
+# print(check_max_distance(11000))
+# print(check_max_distance(999))
 
-report_shell_temperature(three_hundred=37, two_hundred=84, one_hundred=116)
 
-#return types with type hints
-def is_weight_maximum_reached(amount:float):
-    return amount >= 50.57
+# #*args
+# def check_every_pilot_identity(*pilots):
+#     for pilot in pilots:
+#         print(f"{pilot} is confirmed")
 
-print(is_weight_maximum_reached(50.56))
+# check_every_pilot_identity("Goodchild", "Chuck", "Mitch", "Dakota")
+
+# #*kwargs
+# def report_shell_temperature(**degrees):
+#     for star_proximity, value in degrees.items():
+#         print(f"{star_proximity}: {value}")
+
+# report_shell_temperature(three_hundred=37, two_hundred=84, one_hundred=116)
+
+# #return types with type hints
+# def is_weight_maximum_reached(amount:float):
+#     return amount >= 50.57
+
+# print(is_weight_maximum_reached(50.56))
+
+#LIST/DICT COMPREHENSIONS
+#Basic list
+# male_guests = ["Goodchild", "Bashire"]     # list - ordered, allows duplicates
+# male_guests = {"Goodchild", "Bashire"}     # set - unordered, unique only
+# male_guests = ("Goodchild", "Bashire")     # tuple - ordered, immutable
+
+
+male_guests = ("Goodchild", "Bashire")
+female_guests = ("Flux", "Tina")
+
+prefixes = ["Mr " + surname for surname in male_guests]
+print(prefixes)
+
+fprefixes = ["Mrs " + surname for surname in female_guests]
+print(fprefixes)
+
+
+#List comprehension with a filter
+db = ["Di", "Scarlett", "Jetta"]
+short_names = [name for name in db if len(name) <=5]
+print(short_names)
+
+#Dict comprehension
+failed_tests = ["login", "reset_pass", "delete_acc"]
+passed_tests = ["change_role", "add_new_user"]
+fail_results = {test:"fail" for test in failed_tests}
+pass_results = {test: "pass" for test in passed_tests}
+
+print(fail_results)
+print(pass_results)
+
+#List that transforms strings
+capital_letters_names = ["MITCHELL", "MARTINS"]
+adjusted_names = [name.lower() for name in capital_letters_names]
+print(adjusted_names)
