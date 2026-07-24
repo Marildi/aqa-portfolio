afda389fce7d1977be38433c37b7afa9bc8ec167
Marildi <mdidyk.tech@gmail.com>
2026-07-24 08:58:32 -0400

    Step 8: data structures - lists, tuples, discts, sets

--- syntax_practice.py
+++ syntax_practice.py
@@ -1,50 +1,73 @@
-rocketName = "Atlantis"
-totalRocketWeight = 2000.55
-fuelWeight = 1500
-isInOrbit = False
+# rocketName = "Atlantis"
+# totalRocketWeight = 2000.55
+# fuelWeight = 1500
+# isInOrbit = False
 
-# rocketWeight = totalRocketWeight - fuelWeight
-# print(f"The {rocketName} weights {rocketWeight}, fuel weight is {fuelWeight}")
+# # rocketWeight = totalRocketWeight - fuelWeight
+# # print(f"The {rocketName} weights {rocketWeight}, fuel weight is {fuelWeight}")
 
-# hasEnoughFuel = fuelWeight > 1000
-# print(f"Enough fuel: {hasEnoughFuel}, in orbit: {isInOrbit}")
+# # hasEnoughFuel = fuelWeight > 1000
+# # print(f"Enough fuel: {hasEnoughFuel}, in orbit: {isInOrbit}")
 
-#if
+# #if
 
-if not isInOrbit:
-    print("Atlantis not in orbit")
-else:
-    print("Already left")
+# if not isInOrbit:
+#     print("Atlantis not in orbit")
+# else:
+#     print("Already left")
 
-fuelPercent = 45
+# fuelPercent = 45
 
 
-#elif - multiple conditions
+# #elif - multiple conditions
 
-if fuelPercent > 80:
-    print("Fuel: full")
-elif fuelPercent >30:
-    print("Fuel: moderate")
-else:
-    print("Fuel: critical")
+# if fuelPercent > 80:
+#     print("Fuel: full")
+# elif fuelPercent >30:
+#     print("Fuel: moderate")
+# else:
+#     print("Fuel: critical")
 
-#for loop
+# #for loop
 
-systems = ["navigation", "engine", "life_support"]
-for item in systems:
-    print(f"Checking {item}")
+# systems = ["navigation", "engine", "life_support"]
+# for item in systems:
+#     print(f"Checking {item}")
 
-#while loop - runs until condition is false
-countdown = 5
-while countdown > 0:
-    print(countdown)
-    countdown -=1
+# #while loop - runs until condition is false
+# countdown = 5
+# while countdown > 0:
+#     print(countdown)
+#     countdown -=1
 
-#break and continue
+# #break and continue
 
-for system in systems:
-    if system == "navigation":
-        continue
-    if system == "life_support":
-        break
-    print(f"Testing {system}")
+# for system in systems:
+#     if system == "navigation":
+#         continue
+#     if system == "life_support":
+#         break
+#     print(f"Testing {system}")
+
+#LISTS
+#Need order + duplicates + changeable -> list
+domains = ["cybersecurity", "plasma physics", "nuclear physics", "metals science"]
+print(domains[0])
+
+#Need order + fixed, unchangeable → tuple
+metals = ("aluminum", "titanium", "gold", "copper")
+#print(metals)
+metals[0]="flower" #should throw an error test
+
+#Need labeled lookup (like JSON) → dict
+pressureIndicators= {
+    "green":"low",
+    "yellow":"moderate",
+    "red":"high"
+}
+print(pressureIndicators["green"])
+
+#Need uniqueness only → set
+teams = {"supply_chain", "cyber_IT", "cyber_OT", "systems_engineers"}
+if "cyber_IT" in teams:
+    print("On the list!")
