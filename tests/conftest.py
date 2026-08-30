0d438267449416bf773f50a99481d085b50186a7
Marildi <mdidyk.tech@gmail.com>
2026-08-30 13:54:27 -0400

    Fixed the fixture for Actions

--- tests/conftest.py
+++ tests/conftest.py
@@ -63,7 +63,7 @@ def authenticated_page(browser, auth_state):
 
 @pytest.fixture(scope="session")
 def browser_type_launch_args(browser_type_launch_args):
-    return {**browser_type_launch_args, "headless": False}
+    return {**browser_type_launch_args, "headless": True}
 
 
 @pytest.fixture(scope="session")
