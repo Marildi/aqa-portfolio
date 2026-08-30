b2e990d24f14ae07038f78d895e7f234eb676879
Marildi <mdidyk.tech@gmail.com>
2026-08-30 16:44:20 -0400

    Remove deliberate failure now that S3 artifact upload is verified

--- tests/test_s3_file_upload.py
+++
@@ -1,6 +0,0 @@
-from playwright.sync_api import expect
-
-
-def test_deliberate_failure_for_s3_upload_check(page):
-    page.goto("https://www.saucedemo.com")
-    expect(page.get_by_test_id("this-does-not-exist")).to_be_visible()
